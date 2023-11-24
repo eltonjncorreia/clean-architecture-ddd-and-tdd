239698d40efd054f372907b412808f756f8f58e9
eltonjncorreia <eltonjncorreia@gmail.com>
2023-11-23 22:31:19 -0300

    feat: add dtos

--- booking/application/booking/dtos.py
+++ booking/application/booking/dtos.py
@@ -1,2 +1,20 @@
-print("DTO data transfer object")
-print("serve apenas para transportar dados de uma camanda para outra")
+from dataclasses import dataclass
+from datetime import datetime
+
+from booking.domain.booking.entities import Booking
+from booking.domain.customers.entities import Customer
+
+"""
+DTO data transfer object
+serve apenas para transportar dados de uma camanda para outra
+"""
+
+
+@dataclass
+class BookingDTO:
+    checkin: datetime
+    checkout: datetime
+    customer: Customer
+
+    def to_domain(self) -> Booking:
+        return Booking(self.checkin, self.checkout, self.customer)
